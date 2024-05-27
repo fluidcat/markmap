a375b2faafb37ca5fcff160554d2136fff04a91f
fluidcat <Belin0@163.com>
2024-05-27 22:24:25 +0800

    fix bug

--- src/lib/Mindmap.svelte
+++ src/lib/Mindmap.svelte
@@ -158,7 +158,7 @@
 		mm = '<?xml version="1.0" standalone="no"?><!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.0//EN" "http://www.w3.org/TR/2001/REC-SVG-20010904/DTD/svg10.dtd">'
 		+ '<svg id="markmap" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" '
 		+ 'class="' + mindmap.className['baseVal'] + '" '
-		+ 'width="'+boundingBox.w+'" height="'+boundingBox.h+30+'" '
+		+ 'width="'+boundingBox.w+'" height="'+(boundingBox.h+30)+'" '
 		+ 'style="width:100%; height:100%;" viewBox="' + boundingBox.x + ' ' + (boundingBox.y-15) + ' ' + (boundingBox.w) + ' ' + (boundingBox.h+30) + '">'
 		+'<use xlink:href=""><title>'+title+'</title></use>'+'<desc>'+description.replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&lt;')+'</desc>'+ mm.replace(/<title>.*<\/title>/,'') + '</svg>'
 		return mm;
